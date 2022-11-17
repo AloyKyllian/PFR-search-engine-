17ce02eb985b02f41771cb65d202c3d40629cf19
Loic-Maignan <megaloc31860@gmail.com>
2022-11-17 20:32:38 +0100

    Fait Lire_CONFIG et afficher_CONFIG

--- src/Config.c
+++ src/Config.c
@@ -22,28 +22,18 @@ typedef struct
 
 }CONFIG;
 
-CONFIG Lire_Config()
+CONFIG Lire_CONFIG()
 {
     CONFIG config;
     FILE* fichier = NULL;
     int nb_utilisateur = 0;
     int nb_administrateur = 0;
+
+    char chaine[20];
     fichier = fopen("../Config.txt","r");
     if(fichier != NULL)
     {
-        fscanf(fichier, "[1] Nombre de mots clé : %d",&config.nb_mots_cle);
-        fscanf(fichier,"\n[2] Similarité : %d",&config.similariter);
-        fscanf(fichier,"\n\n[3] Niveau : %d",&config.niveau);
-        fscanf(fichier,"\n\n\n\n - Nom1 : %s MDP1 : %s Nom2 : %s MDP2 : %s Nom3 : %s MDP3 : %s Nom4 : %s MDP4 : %s Nom5 : %s MDP5 : %s",&config.nom_utilisateur[0].chaine,&config.nom_utilisateur[1],&config.nom_utilisateur[2],&config.nom_utilisateur[3],&config.nom_utilisateur[4]);
-
-
-
-        fscanf(fichier,"\n\n\n\n\t1- %s %s",&config.nom_utilisateur[0], &config.mdp_utilisateur[0]);
-        fscanf(fichier,"\n\n\n\n\n\t[3] Niveau : %d",&config.niveau);
-
-        printf("%d\n%d\n%d",config.nb_mots_cle,config.similariter,config.niveau);
-        printf("%s",config.nom_utilisateur[0]);
-
+        fscanf(fichier, "[1] Nombre de mots clé : %d\n[2] Similarité : %d\n[3] Niveau : %d\n[4] Informations Utilisateur :\n\t- Nom1 : %s MDP1 : %s Nom2 : %s MDP2 : %s Nom3 : %s MDP3 : %s Nom4 : %s MDP4 : %s Nom5 : %s MDP5 : %s\n[5] Informations Administrateur :\n\t- Nom1 : %s MDP1 : %s Nom2 : %s MDP2 : %s Nom3 : %s MDP3 : %s Nom4 : %s MDP4 : %s Nom5 : %s MDP5 : %s",&config.nb_mots_cle,&config.similariter,&config.niveau,&config.nom_utilisateur[0].chaine,&config.mdp_utilisateur[0].chaine,&config.nom_utilisateur[1].chaine,&config.mdp_utilisateur[1].chaine,&config.nom_utilisateur[2].chaine,&config.mdp_utilisateur[2].chaine,&config.nom_utilisateur[3].chaine,&config.mdp_utilisateur[3].chaine,&config.nom_utilisateur[4].chaine,&config.mdp_utilisateur[4].chaine,&config.nom_admin[0].chaine,&config.mdp_admin[0].chaine,&config.nom_admin[1].chaine,&config.mdp_admin[1].chaine,&config.nom_admin[2].chaine,&config.mdp_admin[2].chaine,&config.nom_admin[3].chaine,&config.mdp_admin[3].chaine,&config.nom_admin[4].chaine,&config.mdp_admin[4].chaine);
     }
     else
     {
@@ -54,7 +44,30 @@ CONFIG Lire_Config()
 
 }
 
+void afficher_CONFIG(CONFIG config)
+{
+    printf("nb_mots_cle = %d\n",config.nb_mots_cle);
+    printf("similariter = %d\n",config.similariter);
+    printf("niveau = %d\n",config.niveau);
+
+    printf("Informations Utilisateur :\n");
+    printf("Nom1 = %s\tMDP1 = %s\n",config.nom_utilisateur[0].chaine,config.mdp_utilisateur[0].chaine);
+    printf("Nom2 = %s\tMDP2 = %s\n",config.nom_utilisateur[1].chaine,config.mdp_utilisateur[1].chaine);
+    printf("Nom3 = %s\tMDP3 = %s\n",config.nom_utilisateur[2].chaine,config.mdp_utilisateur[2].chaine);
+    printf("Nom4 = %s\tMDP4 = %s\n",config.nom_utilisateur[3].chaine,config.mdp_utilisateur[3].chaine);
+    printf("Nom5 = %s\tMDP5 = %s\n",config.nom_utilisateur[4].chaine,config.mdp_utilisateur[4].chaine);
+
+    printf("Informations Administrateur :\n");
+    printf("Nom1 = %s\tMDP1 = %s\n",config.nom_admin[0].chaine,config.mdp_admin[0].chaine);
+    printf("Nom2 = %s\tMDP2 = %s\n",config.nom_admin[1].chaine,config.mdp_admin[1].chaine);
+    printf("Nom3 = %s\tMDP3 = %s\n",config.nom_admin[2].chaine,config.mdp_admin[2].chaine);
+    printf("Nom4 = %s\tMDP4 = %s\n",config.nom_admin[3].chaine,config.mdp_admin[3].chaine);
+    printf("Nom5 = %s\tMDP5 = %s\n",config.nom_admin[4].chaine,config.mdp_admin[4].chaine);
+}
+
 void main()
 {
-    Lire_Config();
+    CONFIG config;
+    config = Lire_CONFIG();
+    afficher_CONFIG(config);
 }
