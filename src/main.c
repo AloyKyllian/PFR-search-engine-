5bf8c26639a03fcfccb736b4f2532c3d1e6ab7da
Youss02 <achachera.youssera@gmail.com>
2022-11-30 22:14:39 +0100

    menu

--- src/main.c
+++ src/main.c
@@ -7,7 +7,133 @@ void main(){
     val=6;
     printf("hello world %d",val);
     //test du merge
-    // menuuuuu 
+    
+
+
+
+    
+    
+}
+
+void Menu_general(){
+    char choix='0';
+    printf("Menu général\n");
+    printf("Veuillez faire votre choix : \n");
+    printf("[1] Administrateur \n[2] Utilissateur \n[Q] Quitter\n");
+    scanf("%c",&choix);
+
+    switch (choix)
+    {
+    case '1' :
+        //admin
+        //Connexion();
+        Menu_Admin();
+        break;
+    case '2' :
+        // utilisatuer 
+        break;  
+    case 'Q' :
+        return ;//???
+        break;
+    default:
+        printf("erreur de choix\n");
+        Menu_general();
+        break;
+    }
 }
 
 
+            void Menu_Admin(){
+                 printf("Menu général de l'administrateur\nFonctionnalité:\n");
+                 char choix;
+                 printf("Veuillez faire votre choix  : ");
+                 printf("[1] Indexation\n [2] Comparaison\n [3] Moteur de recherche \n[R] Retour\n[Q] Déconnexion\n");
+                 scanf("%c",choix);
+
+                 switch (choix){
+                          case '1' :
+                             //Indexation
+                             break;
+                         case '2' :
+                             // Comparaison
+                             break;  
+                         case '3' :
+                             // Moteur de recherche 
+                             //Utilisateur
+                             break;
+                         case 'R' :
+                             Menu_general();
+                             break;
+                         case 'Q' :
+                             return ;//????
+                             break;
+                         default:
+                             return NULL; 
+                             break;
+                        }
+                } 
+
+                 // 1. Indexation 
+                           void Menu_Indexation(){
+                                 printf("Indexation :\n");
+                                 char choix;
+                                 printf("Veuillez faire votre choix  : ");
+                                 printf("[1] Automatique\n [2] ouvrir descripteur\n [3] Configuration \n[R] Retour\n[Q] Déconnexion\n");
+                                 scanf("%c",choix);
+
+                                 switch (choix) {
+                                         case '1' :
+                                             //Lancer l'Indexation automatique
+                                             Menu_Indexation();
+                                             break;
+                                         case '2' :
+                                             // ouvrir descripteur
+                                             Menu_Indexation();
+                                             break;  
+                                         case '3' :
+                                             // Configuration
+                                             Menu_Indexation();
+                                             break;
+                                         case 'R' :
+                                             Menu_Admin();
+                                             break;
+                                         case 'Q' :
+                                         return ;//????
+                                         break;
+                                         default:
+                                         printf("erreur de choix\n");
+                                         Menu_Admin();
+                                         break;
+                                        }
+                             }
+
+                 // 2. Comparaison
+                           void Menu_Comparaison(){
+                                 printf("Comparaison :\n");
+                                 char choix;
+                                 printf("Veuillez faire votre choix  : ");
+                                 printf("[1] \n [2] \n [3] \n[R] Retour\n[Q] Déconnexion\n");
+                                 scanf("%c",choix);
+
+                                 switch (choix) {
+                                         case '1' :
+                                             //
+                                             break;
+                                         case '2' :
+                                             // 
+                                             break;  
+                                         case '3' :
+                                             // 
+                                             break;
+                                         case 'R' :
+                                             Menu_Admin();
+                                             break;
+                                         case 'Q' :
+                                         return ;//????
+                                         break;
+                                         default:
+                                         printf("erreur de choix\n");
+                                         Menu_Admin();
+                                         break;
+                                        }
+                             }
