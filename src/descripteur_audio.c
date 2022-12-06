01f54675f9b55854604d0d740bdae90b6954a8b7
thepandou05 <aloykyllian31520@gmail.com>
2022-12-06 15:49:25 +0100

    ajout d'une var char

---
+++ src/descripteur_audio.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "Global.h"
+
+char Descripteur_test(){
+    float val;
+    FILE *fichier = NULL;
+    char texte[20];
+
+
+    while(fscanf(fichier,"%f",&val)!=NULL){
+
+
+
+    }
+
+
+
+}
