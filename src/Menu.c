8317a1d48512cf19e8e1fd28529745f4eba4663c
Youss02 <achachera.youssera@gmail.com>
2022-12-13 19:26:43 +0100

    Menu Sans appel de fonction

--- src/Menu.c
+++ src/Menu.c
@@ -20,8 +20,7 @@
 //Menu Admin
 #define Indexation 1
 #define Menu_Configuration 2
-#define Visualisation 3
-#define Menu_Visualition 2
+#define Menu_Visualisation 3
 #define Retour_Utilisateur 4
 
 //Menu configuration 
@@ -38,16 +37,15 @@
 #define audio 3
 
 //Menu recherche texte
-#define Recherche par mots clé 1
-#define Recherche par comparaison textes 2
+#define Recherche_mots_cle 1
+#define Recherche_par_comparaison 2
 #define Retour 'R'
 
 //Menu recherche image
-#define Recherche par nom couleur 1
-#define Recherche par comparaison imges 2
+#define Recherche_nom_couleur 1
 
 //Menu recherche audio
-#define Recherche par comparaison audio 1
+#define Recherche_comparaison_audio 1
 char choix;
 
 
@@ -100,13 +98,13 @@ void MAE(){
                         // Configuration
                         etat_courant=Menu_Configuration;
                         break;  
-                 case Visualisation :
+                 case Menu_Visualisation :
                         //visualisation
                         break;
                  case Retour_Utilisateur :
                         etat_courant=Menu_Utilisateur;
                         break;
-                 case 'Q' :
+                 case Quitter :
                         return ;//????
                         break;
                  default:
@@ -114,6 +112,8 @@ void MAE(){
                         etat_courant=Menu_Admin;
                         break;
                         }
+        break;
+        
         case Menu_Configuration :
              printf("Configuration :\n");
              printf("Veuillez faire votre choix  : \n");
@@ -136,154 +136,153 @@ void MAE(){
                     case Intervalle_de_temps :
                         //fct pour changer l'intervalle de temps
                     case Retour :
-                        
-
-                     
-                    case 'Q' :
+                        etat_courant=Menu_Admin;
+                    case Quitter :
                         return ;//????
                         break;
                     default:
                         printf("erreur de choix\n");
-                        etat_courant=Menu_Admin;
+                        etat_courant=Menu_Configuration;
                         break;
-                        }                     
-                                        }
+                        }  
+        break;
+
+        case Menu_Visualisation :
+             printf("Visualisation des recueils des descripteurs :\n");
+             printf("Veuillez faire votre choix  : \n");
+             printf("[1] Texte\n [2] Image\n [3] Audio \n[R] Retour\n[Q] Déconnexion\n");
+             scanf("%c",choix);
+             switch (choix) {
+                    case texte :
+                         //                  
+                         break;
+                    case image :
+                         // 
+                         break;  
+                    case audio :
+                         //
+                         break;
+                    case Retour :
+                        etat_courant=Menu_Admin;
+                    case Quitter :
+                        return ;//????
+                        break;
+                    default:
+                        printf("erreur de choix\n");
+                        etat_courant=Menu_Visualisation;
+                        break;
+                        }  
+        break;
+
+        case Menu_Utilisateur :
+             printf("Menu utilisateur :\n");
+             printf("Veuillez faire votre choix  : \n");
+             printf("[1] Recherche fichier texte\n [2] Recherche fichier image\n [3] Recherche fichier audio \n[Q] Déconnexion\n");
+             scanf("%c",choix);
+             switch (choix) {
+                    case texte :
+                         //                  
+                         break;
+                    case image :
+                         // 
+                         break;  
+                    case audio :
+                         //
+                         break;
+                    case Retour :
+                        etat_courant=Menu_Utilisateur;
+                        break;
+                    case Quitter :
+                        return ;//????
+                        break;
+                    default:
+                        printf("erreur de choix\n");
+                        etat_courant=Menu_Utilisateur;
+                        break;
+                        }  
+        break;
+
+        case texte :
+             printf("Recherche fichier texte :\n");
+             printf("Veuillez faire votre choix  : \n");
+             printf("[1] Recherche par mot clé\n [2] Recherche par comparaison de textes\n[R] Retour\n[Q] Quitter\n");
+             scanf("%c",choix);
+             switch (choix) {
+                    case Recherche_mots_cle :
+                        //                  
+                        break;
+                    case Recherche_par_comparaison :
+                        // 
+                        break;  
+                    case Retour :
+                        etat_courant=Menu_Utilisateur;
+                        break;
+                    case Quitter :
+                        return ;//????
+                        break;
+                    default:
+                        printf("erreur de choix\n");
+                        etat_courant=texte;
+                        break;
+                        }  
+        break;
+
+        case image :
+             printf("Recherche fichier image :\n");
+             printf("Veuillez faire votre choix  : \n");
+             printf("[1] Recherche par nom de couleur\n [2] Recherche par comparaison d'images\n[R] Retour\n[Q] Quitter\n");
+             scanf("%c",choix);
+             switch (choix) {
+                    case Recherche_nom_couleur :
+                        //                  
+                        break;
+                    case Recherche_par_comparaison :
+                        // 
+                        break;  
+                    case Retour :
+                        etat_courant=Menu_Utilisateur;
+                        break;
+                    case Quitter :
+                        return ;//????
+                        break;
+                    default:
+                        printf("erreur de choix\n");
+                        etat_courant=image;
+                        break;
+                        }  
+        break;
+        
+        case audio :
+             printf("Recherche fichier audio :\n");
+             printf("Veuillez faire votre choix  : \n");
+             printf("[1] Recherche par comparaison d'audio\n[R] Retour\n[Q] Quitter\n");
+             scanf("%c",choix);
+             switch (choix) {
+                    case Recherche_par_comparaison :
+                        // 
+                        break;  
+                    case Retour :
+                        etat_courant=Menu_Utilisateur;
+                        break;
+                    case Quitter :
+                        return ;//????
+                        break;
+                    default:
+                        printf("erreur de choix\n");
+                        etat_courant=audio;
+                        break;
+                        }  
+        break;
+
+
+
+
+          
+
+                                        
     break;
     default:
     break;
+      }
     }
-
-}
-
-void Menu_general(){
-    char General='0';
-    printf("Menu général\n");
-    printf("Veuillez faire votre choix : \n");
-    printf("[1] Administrateur \n[2] Utilisateur \n[Q] Quitter\n");
-    scanf("%c",&General);
-    
-    switch (General)
-    {
-    case Administrateur :
-        //admin
-        //Connexion();
-        Menu_Admin();
-        break;
-    case '2' :
-        // utilisateur 
-        break;  
-    case 'Q' :
-        printf("vous avez quitté le programme\n");
-        break;
-    default:
-        printf("erreur de choix\n");
-        Menu_general();
-        break;
-    }
-}
-
-
-            void Menu_Admin(){
-                 printf("Menu général de l'administrateur\nFonctionnalité :\n");
-                 char choix;
-                 printf("Veuillez faire votre choix  : \n");
-                 printf("[1] Indexation\n [2] Comparaison\n [3] Moteur de recherche \n[R] Retour\n[Q] Déconnexion\n");
-                 scanf("%c",choix);
-
-                 switch (choix){
-                          case '1' :
-                             //Indexation
-                             break;
-                         case '2' :
-                             // Comparaison
-                             break;  
-                         case '3' :
-                             // Moteur de recherche 
-                             //Utilisateur
-                             break;
-                         case 'R' :
-                             Menu_general();
-                             break;
-                         case 'Q' :
-                             return ;//????
-                             break;
-                         default:
-                             return NULL; 
-                             break;
-                        }
-                } 
-
-                 // 1. Indexation 
-                           void Menu_Indexation(){
-                                 printf("Indexation :\n");
-                                 char choix;
-                                 printf("Veuillez faire votre choix  : \n");
-                                 printf("[1] Automatique\n [2] ouvrir descripteur\n [3] Configuration \n[R] Retour\n[Q] Déconnexion\n");
-                                 scanf("%c",choix);
-
-                                 switch (choix) {
-                                         case '1' :
-                                             //Lancer l'Indexation automatique
-                                             Menu_Indexation();
-                                             break;
-                                         case '2' :
-                                             // ouvrir descripteur
-                                             Menu_Indexation();
-                                             break;  
-                                         case '3' :
-                                             // Configuration
-                                             Menu_Indexation();
-                                             break;
-                                         case 'R' :
-                                             Menu_Admin();
-                                             break;
-                                         case 'Q' :
-                                         return ;//????
-                                         break;
-                                         default:
-                                         printf("erreur de choix\n");
-                                         Menu_Admin();
-                                         break;
-                                        }
-                             }
-
-                 // 2. Comparaison
-                           void Menu_Comparaison(){
-                                 printf("Comparaison :\n");
-                                 char choix;
-                                 char choix2;
-                                 printf("Veuillez faire votre choix  : ");
-                                 printf("[1] Texte\n [2] Image\n [3] Audio\n[R] Retour\n[Q] Déconnexion\n");
-                                 scanf("%c",&choix);
-
-                                 if( (choix=='1') || (choix=='2') || (choix='3')){
-                                     printf("Veuillez faire votre choix  : ");
-                                     printf("[1] Fichiers\n [2] Descripteurs\n[R] Retour\n[Q] Déconnexion\n");
-                                     scanf("%c",choix2);
-                                 } 
-                                 switch (choix) {
-                                         case '1' :
-                                             //Texte
-                                             
-                                             break;
-                                         case '2' :
-                                             // Image
-
-                                             break;  
-                                         case '3' :
-                                             // Audio
-
-                                             break;
-                                         case 'R' :
-                                             Menu_Admin();
-                                             break;
-                                         case 'Q' :
-                                         return ;//????
-                                         break;
-                                         default:
-                                         printf("erreur de choix\n");
-                                         Menu_Admin();
-                                         break;
-                                        }
-                             }
 
