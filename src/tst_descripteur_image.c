64cb4594827b038534fe78bab11c4115729d2e2d
Loic-Maignan <megaloc31860@gmail.com>
2022-12-13 17:10:51 +0100

    Modif du test

--- src/tst_descripteur_image.c
+++ src/tst_descripteur_image.c
@@ -3,12 +3,13 @@
 
 void main()
 {
+    // Variable
     IMAGE img;
     DESCRIPTEUR_IMAGE descripteur_image;
     String Erreur;
     String Path = "../DATA_FIL_ROUGE_DEV/TEST_RGB/01.txt";
-    int niveau = 2;
-    int total =0;
+    int Nb_Bit_Fort = 8;
+    int total = 0;
 
     printf("\nLecture de la matrice\n");
     img = Lire_image(&Erreur,Path);
@@ -16,15 +17,20 @@ void main()
     printf("1er element rouge = %d\n1er element vert = %d\n1er element bleu = %d\n",img.adr_Matrice[0][0],img.adr_Matrice[img.Nb_Ligne][0],img.adr_Matrice[img.Nb_Ligne*2][0]);
 
     printf("\nPretraitement\n");
-    img = Pre_traitement(&Erreur,img,2);
+    img = Pre_traitement(&Erreur,img,Nb_Bit_Fort);
     printf("Nb_composante = %d\n",img.Nb_composante);
     printf("1er element = %d\n",img.adr_Matrice[0][0]);
 
-    descripteur_image = Creation_Discripteur(&Erreur,img,2);
+    descripteur_image = Creation_Discripteur(&Erreur,img,Nb_Bit_Fort);
     printf("\nAffichage du tableau descripteur\n");
-    for(int i =0; i < pow(2,niveau*img.Nb_composante); i++)
+    for(int i = 0; i < pow(2,Nb_Bit_Fort*img.Nb_composante); i++ )
     {
-        printf("Val = %d Quantity = %d\n",descripteur_image.Bilan[i][0],descripteur_image.Bilan[i][1]);
+        // Permet d'afficher q'un certain nombre de valeur
+        if( (i % (int)(0.1 *pow(2,Nb_Bit_Fort*img.Nb_composante))) == 0)
+        {
+            printf("Val n°%d = %d Quantity = %d\n",i,descripteur_image.Bilan[i][0],descripteur_image.Bilan[i][1]);
+        }
+       
         total = total+descripteur_image.Bilan[i][1];
     }
     printf("\nTotal de valeur = %d\n",total);
