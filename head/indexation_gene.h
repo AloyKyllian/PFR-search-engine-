29e3b5c3086d8905db573b16f9cde04d1d92e75d
yasminaallal <all.yasmine01@gmail.com>
2022-12-15 18:40:44 +0100

    commentaire .h de ce que je dois faire

---
+++ head/indexation_gene.h
@@ -0,0 +1,23 @@
+#ifndef INDEXATIN_GENE_H
+#define INDEXATION_GENE_H
+
+#include "../head/Global.h"
+
+
+// 1 // fonction pour recuperer chemin d'acces de chaque type de fichier et stocker resultat dans un tableau
+              // appeler trois fois main pour chaque type de fichier
+              // avoir trois tableaus de string
+
+// 2 // fonction recuperer leurs structures et lempiler dans une pile
+             //appeler leur fonctions descripteur
+              //empiler resultat
+              //appeler fonctions trois fois pour avoir le resultat
+
+// 3 // fonction depiler dans un fichier texte
+            // depiler les piles empiler dans un fichier texte
+            // avoir le receuil des descripteurs
+
+// 4 // fonction pour link les ids et les chemins d'acces
+
+
+#endif
