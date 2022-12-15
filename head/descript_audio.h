085615e2f7a2859a6df3c9623dd03946bf831718
Kyllian Aloy <aloykyllian31520@gmail.com>
2022-12-15 20:04:59 +0100

    ajout des commentaires et creation du .h

---
+++ head/descript_audio.h
@@ -0,0 +1,15 @@
+#ifndef DESCRIPT_AUDIO_H
+#define DESCRIPT_AUDIO_H
+
+#include "../head/Global.h"
+
+typedef struct
+{
+    int size_x;
+    int size_y;
+    int **tab;
+} descri_audio;
+
+descri_audio Descripteur_audio(int fenetre, int intervalle, char *chemin_fichier, descri_audio desci);
+
+#endif
--- src/descript_audio.c
+++ src/descript_audio.c
@@ -1,108 +1,97 @@
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "../head/Global.h"
+#include "../head/descript_audio.h"
 
-
-int ** Descripteur_audio(int fenetre,int intervalle,char *chemin_fichier,int** tableau){//demander comment recuperer le chemin et nom du fichier
+descri_audio Descripteur_audio(int fenetre,int intervalle,char *chemin_fichier,descri_audio desci){//demander comment recuperer le chemin et nom du fichier
     double pas;
-    int fe=0;//(nombre de fenetre)
     int nbr_val=0;
     int k;//(nbr_val_fenetre)
-    int m;
-    int i=0;
-    double val;
-    FILE* fichier = NULL;
-    char c;
+    int m;//nombre d'intervalle
+    double val;//valeur lu dans le fichier
+    FILE* fichier = NULL;//pointeur de fichier pour utiliser les fonction associé
 
-    //chemin_fichier="../son/jingle_fi.txt";
-    fichier = fopen(chemin_fichier, "r");
-    if(fichier==NULL){                           
+    //chemin_fichier="../son/jingle_fi.txt";//example de chemin possible
+    fichier = fopen(chemin_fichier, "r");//ouvre le fichier ne mode read
+    if(fichier==NULL){              
         printf("Erreur lors de l'ouverture d'un fichier");
         exit(1);
     }
     while(fscanf(fichier,"%lf",&val)!=EOF){
         nbr_val++;
     }
-    k=(nbr_val/fenetre)+1;
-    tableau = malloc(k * sizeof(*tableau));
-    for (int i = 0; i < k; i++)
+    
+    k=(nbr_val/fenetre)+1;//calcul du nombre de fenetre
+
+    desci.size_x=k;
+    desci.size_y=intervalle;
+    desci.tab = malloc(k * sizeof(*desci.tab));//creation du tableau
+    for (int i = 0; i <= k; i++)
     {
-        tableau[i] = malloc(intervalle * sizeof(**tableau));
+        desci.tab[i] = malloc(intervalle * sizeof(**desci.tab));
     }
 
-    for (unsigned i = 0; i < k; ++i)
+    for (unsigned i = 0; i < k; ++i)//initialisation du tableau(possibilité de fonction)
     {
         for (unsigned j = 0; j < intervalle; ++j)
         {
-            printf(" |%3d| ",tableau[i][j]);
+            desci.tab[i][j]=0;
         }
-        printf("\r\n");
     }
     k=0;
     pas=2./intervalle;
-    rewind(fichier);
-    while(fscanf(fichier,"%lf",&val)!=EOF){
+    rewind(fichier);//permet de revenir au debut du fichier
 
-        if(i==fenetre)
+    for(int cpt=0;fscanf(fichier,"%lf",&val)!=EOF;cpt++)
+    {
+        if(cpt==fenetre)//passer de fenetre en fenetre
         {
-            i=0;
+            cpt=0;
             k++;
         }
 
-        if(nbr_val<=fenetre)
+        if(nbr_val<fenetre)//verifier si on est au dans les derniere valeurs pour creer une fenetre bonus
         {
-            for(m=0;-1+m*pas<1;m++)
-            {
-
+            for(m=0;-1+m*pas<1;m++)//comptage des valeurs
+            {                 
                 if(val>=pas*m-1 && val<pas*(m+1)-1)
                 {
-
                     if(val<1&&val>-1&&m<intervalle)
-                        tableau[k][m]++;
+                        desci.tab[k+1][m]++;
                 }
-                
             }
+           
         }
-          
+
         for(m=0;m<intervalle;m++)
         {
             if((val>=(pas*m-1)) && (val<(pas*(m+1)-1)))
             {
-             
                 if(val<1&&val>-1&&m<intervalle)
-                    tableau[k][m]++;
+                    desci.tab[k][m]++;
             }
                
         }
-        i++;
         nbr_val--;
     }
-    fclose(fichier);
+    fclose(fichier);//fermeture du fichier
+
+return desci;//retour de la structure du descripteur
+}
 
 
-    for (unsigned i = 0; i < k; ++i)
+
+/*
+void main(){//exemple d'utilisation
+  descri_audio mabite;
+
+    mabite=Descripteur_audio(2048,32,"../son/jingle_fi.txt",mabite);
+    
+    for (unsigned i = 0; i < mabite.size_x; ++i)
     {
-        for (unsigned j = 0; j < m; ++j)
+        for (unsigned j = 0; j < mabite.size_y; ++j)
         {
-            printf(" |%3d| ",tableau[i][j]);
+            printf(" |%3d| ",mabite.tab[i][j]);
         }
         printf("\r\n");
     }
-return tableau;
-}
-
-
-
-
-void main(){
-  int **mabite;
-
-    mabite=Descripteur_audio(2048,32,"../son/corpus_fi.txt",mabite);
-
-    //printf("%d",mabite[5][15]);
 
 }
-
-
-
+*/
