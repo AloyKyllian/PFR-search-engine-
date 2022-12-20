ecf499644e322c0dceca02f83418abab1744c7d6
yasminaallal <all.yasmine01@gmail.com>
2022-12-20 14:46:59 +0100

    continuité du programme

---
+++ src/testindexgeneral.c
@@ -0,0 +1,9 @@
+#include "../head/indexation_gene.h"
+
+void main()
+{
+    String path;
+    String *Erreur;
+    receuillefichiers doctexte;
+    recup_path(doctexte, String path, String *Erreur);
+}
