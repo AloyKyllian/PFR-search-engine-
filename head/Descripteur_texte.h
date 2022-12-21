3686104a94655bf8e33e9b7401f3cb26de25a789
Ryukamis <hugo.lestrade1610@gmail.com>
2022-12-21 15:55:46 +0100

    Test descripteur texte

---
+++ head/Descripteur_texte.h
@@ -0,0 +1,17 @@
+#ifndef DESCRIPTEUR_TEXTE_H
+#define DESCRIPTEUR_TEXTE_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include "../head/Global.h"
+#include <string.h>
+#include <ctype.h>
+
+typedef struct 
+{
+    String *tab_mot;
+    int *tab_app;
+    int index;
+}tab_total;
+
+int comptemot(char* mot_lu);
