14f302e42f2ed0c7d7cf02b2daa301a1f10acf51
Loic-Maignan <megaloc31860@gmail.com>
2022-12-22 13:33:57 +0100

    tst

--- Comparaison-image/src/Comparaison-image.c
+++ Comparaison-image/src/Comparaison-image.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Comparaison_image()
+typedef char String[100];
+typedef struct
+{
+    String ID;
+    String Path;
+    int **Bilan;
+} DESCRIPTEUR_IMAGE;
+
+void Comparaison_image(DESCRIPTEUR_IMAGE Nv_Des, DESCRIPTEUR_IMAGE Ac_Des)
+{
+    
+}
