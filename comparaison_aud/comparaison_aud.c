aed24bcafa040dbe905ea8f524206a5bd0cd102f
thepandou05 <aloykyllian31520@gmail.com>
2023-01-03 09:36:40 +0100

    ajout de teste

--- comparaison_aud/comparaison_aud.c
+++ comparaison_aud/comparaison_aud.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "/head/descript_audio.h"
+#include "../head/descript_audio.h"
 
 
 
@@ -20,9 +20,41 @@ void comparaison_audio(int fenetre,int intervalle,descri_audio descri,char* chem
     int col=0;
     int bonneval=0;
     float pourcentage;
+
+int ligne=0;
+int colonne=0;
+    descri_audio descripteur_comparé;
+    descripteur_comparé.tab = malloc(10000 * sizeof(*descripteur_comparé.tab));//creation du tableau
+    for (int i = 0; i <= 10000; i++)
+    {
+        descripteur_comparé.tab[i] = malloc(10000 * sizeof(**descripteur_comparé.tab));
+    }
+
+
+
     while(fscanf(fichier,"%d",&val_lu)!=EOF)//permet de sauter l'entet du fichier
     {
-        if(nbr_val%intervalle==0)//renvient a la ligne 
+        while(val_lu>=0)
+        {
+            descripteur_comparé.tab[ligne][colonne]=val_lu;
+            ligne++;
+            colonne++;
+        }
+
+    for (unsigned i = 0; i < ligne; ++i)
+    {
+        for (unsigned j = 0; j < colonne; ++j)
+        {
+            printf(" |%3d| ",descripteur_comparé.tab[i][j]);
+        }
+        printf("\r\n");
+    }
+
+
+
+
+
+        /*if(nbr_val%intervalle==0)//renvient a la ligne 
         {
             col=0;
             lig++;
@@ -43,7 +75,7 @@ void comparaison_audio(int fenetre,int intervalle,descri_audio descri,char* chem
         {
             bonneval++;
         }
-        nbr_val++;
+        nbr_val++;*/
     }
 
 
@@ -59,6 +91,56 @@ void comparaison_audio(int fenetre,int intervalle,descri_audio descri,char* chem
 
 void main (){
 
+
 printf("hello world");
+    int nbr_val;
+    int val_lu;
+    FILE* fichier = NULL;
+    fichier = fopen("../descrripteur_texte_type.txt", "r");
+    if(fichier==NULL){
+        printf("Erreur lors de l'ouverture d'un fichier");
+        exit(1);
+    }
+    int lig=0;
+    int col=0;
+    int bonneval=0;
+    float pourcentage;
+
+int ligne=0;
+int colonne=0;
+    descri_audio descripteur_comparé;
+    descripteur_comparé.tab = malloc(10000 * sizeof(*descripteur_comparé.tab));//creation du tableau
+    for (int i = 0; i <= 10000; i++)
+    {
+        descripteur_comparé.tab[i] = malloc(10000 * sizeof(**descripteur_comparé.tab));
+    }
+
+
+    while(fscanf(fichier,"%d",&val_lu)!=EOF)//permet de sauter l'entet du fichier
+    {
+        printf("%d\n",val_lu);
+        
+        while(val_lu>=0)
+        {
+            //descripteur_comparé.tab[ligne][colonne]=val_lu;
+            printf("%d\n",val_lu);
+            ligne++;
+            colonne++;
+            fscanf(fichier,"%d",&val_lu);
+        }
+/*
+    for (unsigned i = 0; i < ligne; ++i)
+    {
+        for (unsigned j = 0; j < colonne; ++j)
+        {
+            printf(" |%3d| ",descripteur_comparé.tab[i][j]);
+        }
+        printf("\r\n");
+    }
+
+
+printf("hello world");
+*/
+}
 
 }
