738fb64280deb7e3509acc50675a44f87e0e28e5
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-01-04 08:54:11 +0100

    push tempo

--- comparaison_aud/comparaison_aud.c
+++ comparaison_aud/comparaison_aud.c
@@ -2,84 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../head/descript_audio.h"
+#include "../descript_audio/descript_audio.h"
 
 
+void comparaison(int val_lu,descri_audio descripteur_comparé,int *ligne,int *colonne,int intervalle,descri_audio descri,float fenetre){
 
-void comparaison_audio(int fenetre,int intervalle,descri_audio descri,char* chemin_descripteur_audio){
-
-    int nbr_val;
-    int val_lu;
-    FILE* fichier = NULL;
-    fichier = fopen(chemin_descripteur_audio, "r");
-    if(fichier==NULL){
-        printf("Erreur lors de l'ouverture d'un fichier");
-        exit(1);
-    }
-    int lig=0;
-    int col=0;
-    int bonneval=0;
+    int id=val_lu;
     float pourcentage;
-
-int ligne=0;
-int colonne=0;
-    descri_audio descripteur_comparé;
-    descripteur_comparé.tab = malloc(10000 * sizeof(*descripteur_comparé.tab));//creation du tableau
-    for (int i = 0; i <= 10000; i++)
+    descripteur_comparé.size_x=*ligne;
+    descripteur_comparé.size_y=intervalle;
+    descri_audio descripteur_intervale;
+    descripteur_intervale.tab = malloc(descri.size_x * sizeof(*descripteur_intervale.tab));//creation du tableau
+    for (int i = 0; i <= descri.size_x; i++)
     {
-        descripteur_comparé.tab[i] = malloc(10000 * sizeof(**descripteur_comparé.tab));
+        descripteur_intervale.tab[i] = malloc(descri.size_y * sizeof(**descripteur_intervale.tab));
+    }
+    
+    for(int intnbr=0;intnbr<=descripteur_comparé.size_x-descri.size_x;intnbr++)
+    {int comparateur=0;
+        for (unsigned i = 0; i < descri.size_x; ++i)
+        {
+            for (unsigned j = 0; j < descri.size_y; ++j)
+                {
+                    if(descripteur_comparé.tab[i+intnbr][j]-descri.tab[i][j]>0)
+                        comparateur+=descripteur_comparé.tab[i+intnbr][j]-descri.tab[i][j];
+                    else
+                        comparateur+=descri.tab[i][j]-descripteur_comparé.tab[i+intnbr][j];
+                    //printf("%d   ",comparateur);
+                }
+                //printf("\r\n");
+        }
+        pourcentage=comparateur/(fenetre*4.)*100;
+        pourcentage=100-pourcentage;
+        printf("\n pourcentage : %f   \n",pourcentage);
     }
 
-
-
-    while(fscanf(fichier,"%d",&val_lu)!=EOF)//permet de sauter l'entet du fichier
-    {
-        while(val_lu>=0)
-        {
-            descripteur_comparé.tab[ligne][colonne]=val_lu;
-            ligne++;
-            colonne++;
-        }
-
-    for (unsigned i = 0; i < ligne; ++i)
-    {
-        for (unsigned j = 0; j < colonne; ++j)
-        {
-            printf(" |%3d| ",descripteur_comparé.tab[i][j]);
-        }
-        printf("\r\n");
-    }
-
-
-
-
-
-        /*if(nbr_val%intervalle==0)//renvient a la ligne 
-        {
-            col=0;
-            lig++;
-        }
-        if(descri.size_x<lig)
-        {
-            lig=0;
-        }
-        if(val_lu<0)//nouveau descripteur
-        {
-           pourcentage = bonneval/nbr_val*100;
-
-            nbr_val=0;
-            col=0;
-            lig=0;
-        }
-        if(val_lu==descri.tab[lig][col])//valeur bonne
-        {
-            bonneval++;
-        }
-        nbr_val++;*/
-    }
-
-
-
+    *ligne=0;
+    *colonne=0;
 
 
 
@@ -88,11 +47,8 @@ int colonne=0;
 
 
 
+void comparaison_audio(int fenetre,int intervalle,descri_audio descri,char* chemin_descripteur_audio){
 
-void main (){
-
-
-printf("hello world");
     int nbr_val;
     int val_lu;
     FILE* fichier = NULL;
@@ -104,43 +60,80 @@ printf("hello world");
     int lig=0;
     int col=0;
     int bonneval=0;
-    float pourcentage;
 
-int ligne=0;
-int colonne=0;
-    descri_audio descripteur_comparé;
-    descripteur_comparé.tab = malloc(10000 * sizeof(*descripteur_comparé.tab));//creation du tableau
+
+    int ligne=0;
+    int colonne=0;
+
+    int fin;
+    descri_audio descripteur_compare;
+    descripteur_compare.tab = malloc(10000 * sizeof(*descripteur_compare.tab));//creation du tableau
     for (int i = 0; i <= 10000; i++)
     {
-        descripteur_comparé.tab[i] = malloc(10000 * sizeof(**descripteur_comparé.tab));
+        descripteur_compare.tab[i] = malloc(intervalle * sizeof(**descripteur_compare.tab));
     }
 
 
-    while(fscanf(fichier,"%d",&val_lu)!=EOF)//permet de sauter l'entet du fichier
+
+
+
+    while(fin!=EOF)
     {
-        printf("%d\n",val_lu);
-        
-        while(val_lu>=0)
+        fin=fscanf(fichier,"%d",&val_lu);
+        if(val_lu<0 && val_lu!=-1)
         {
-            //descripteur_comparé.tab[ligne][colonne]=val_lu;
-            printf("%d\n",val_lu);
-            ligne++;
+           comparaison( val_lu, descripteur_compare, &ligne, &colonne, intervalle, descri, fenetre);
+        }
+           
+
+        if(val_lu>=0)
+        {
+            descripteur_compare.tab[ligne][colonne]=val_lu;
+            //printf(" %d ",descripteur_compare.tab[ligne][colonne]);
             colonne++;
-            fscanf(fichier,"%d",&val_lu);
+            if(colonne==32)
+            {
+                //printf("\n");
+                colonne=0;
+                ligne++;
+            }
         }
-/*
-    for (unsigned i = 0; i < ligne; ++i)
+}
+fclose(fichier);
+    descripteur_compare.size_x=ligne;
+    descripteur_compare.size_y=intervalle;
+
+
+comparaison( val_lu, descripteur_compare, &ligne, &colonne, intervalle, descri, fenetre);
+
+}
+
+
+
+
+
+void main (){
+
+
+    descri_audio descri;
+    descri.size_x=2;
+    descri.size_y=32;
+    descri.tab = malloc(2 * sizeof(*descri.tab));//creation du tableau
+    for (int i = 0; i <= 2; i++)
     {
-        for (unsigned j = 0; j < colonne; ++j)
-        {
-            printf(" |%3d| ",descripteur_comparé.tab[i][j]);
-        }
-        printf("\r\n");
+        descri.tab[i] = malloc(32 * sizeof(**descri.tab));
     }
 
 
-printf("hello world");
-*/
-}
+                for (unsigned i = 0; i < descri.size_x; ++i)
+            {
+                for (unsigned j = 0; j < descri.size_y; ++j)
+                    {
+                        descri.tab[i][j]=64;
+                    }
+                    printf("\r\n");
+            }
+
+comparaison_audio(2048,32,descri,"f");
 
 }
