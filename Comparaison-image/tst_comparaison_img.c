d44716ae9d6b45be5d3b8eff33f6131f44522237
Loic-Maignan <megaloc31860@gmail.com>
2023-01-08 11:40:37 +0100

    modif merge +tst

--- Comparaison-image/tst_comparaison_img.c
+++ Comparaison-image/tst_comparaison_img.c
@@ -4,5 +4,6 @@ void main()
 {
     tab_similaire *tab;
     int erreur;
+    printf("Test de comparaison de 1 image a 2 autres\nLa 1er doit etre identique");
     tab = Comparaison_descripteur_image(&erreur,"Recueil_descripteur.txt","../descripteur_image/DATA_FIL_ROUGE_DEV/TEST_RGB/01.txt",2);
 }
