63caf270d9429ff35c1c7196f9255d93b4c87c7d
Youss02 <achachera.youssera@gmail.com>
2023-01-12 12:17:09 +0100

    prog pour lire et affiche resultat recherche

--- Menu/LireResultat copy.c
+++ Menu/LireResultat copy.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct {
+    int id;
+    int pourcentage;
+}tabRes;
 
-void LireResultat(){
+void LireResultat(tabRes *tabResultat, int nbElement){
+    //recupere les textes grace au ID 
+    //les stocker dans le fichier a la place des ID
+    //afficher 
+    //ouvrir le premier fichier
+    //voir si l'utilisaeur veut ouvrir un autre
+    for(int k=0;k<nbElement;k++){
+		printf("ID=%d, nb=%d\n",tabResultat[k].id,tabResultat[k].pourcentage);
+	}
+
+
     
 }
