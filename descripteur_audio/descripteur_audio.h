4371e77ab3120f55ccec61f48a83aeb6743e3468
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-01-14 09:16:58 +0100

    ajout de desrcipteur audio

---
+++ descripteur_audio/descripteur_audio.h
@@ -0,0 +1,43 @@
+#ifndef DESCRIPT_AUDIO_H
+#define DESCRIPT_AUDIO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    int ligne;
+    int colonne;
+    int **tab;
+} descri_audio;
+
+
+/**
+ * @brief 
+ * 
+ * elle permet de creer un descripteur d'un fichier txt tirer d'un fichier audio
+ * 
+ * @param fenetre 
+ * @param intervalle 
+ * @param chemin_fichier 
+ * @param erreur 
+ * @return descri_audio 
+ * 
+ * 
+ * 
+ */
+descri_audio Descripteur_audio(int fenetre, int intervalle, char *chemin_fichier,int * erreur);
+
+/**
+ * @brief 
+ * 
+ * a quoi ellesert
+ * 
+ * @param path chemin du fichier
+ * @param erreur 
+ * @return int 
+ */
+int getligne(char *path, int *erreur);
+
+#endif
