f2e9a21f10fcfe4faae81b7a451b59209fd386c7
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-01-23 17:15:27 +0100

    comm pour youss

--- RechercheMot/rechercheMot.h
+++ RechercheMot/rechercheMot.h
@@ -3,5 +3,15 @@
 #include "../Gestion-Config/Global.h"
 #include "../comparaison/comparaison.h"
 
+/**
+ * @brief
+ *
+ * @param mot
+ * @param chemin
+ * @param tabResultat
+ * @param nb_mot
+ * @param nbElTab
+ * @param erreur
+ */
 void rechercheMot(char *mot, char *chemin, tab_similaire *tabResultat, int nb_mot, int *nbElTab, int *erreur);
 #endif
