5f247f79a1f07584f86d846a925dbba45b8152ec
Kyllian Aloy <aloykyllian31520@gmail.com>
2023-01-23 09:10:10 +0100

    commentaire kyllian

--- comparaison/comparaison.h
+++ comparaison/comparaison.h
@@ -15,18 +15,36 @@ typedef struct
     float pourcentage;
 } tab_similaire;
 
-tab_similaire* Comparaison_descripteur_image(int *Erreur, char PathRecueil[], char PathNvImg[], int Nb_Bits_Fort, int *Nb_des);
+tab_similaire *Comparaison_descripteur_image(int *Erreur, char PathRecueil[], char PathNvImg[], int Nb_Bits_Fort, int *Nb_des);
 
-
+/**
+ * @brief
+ *
+ */
 float comparaison(int val_lu, descri_audio descripteur_comparé, int ligne, int intervalle, descri_audio descri, float fenetre);
 
+/**
+ * @brief
+ *
+ * Elle utilise la fonction "getligne" pour obtenir le nombre de lignes dans le fichier à comparer,
+ * crée un descripteur audio pour ce fichier en utilisant la fonction "Descripteur_audio",
+ * alloue de la mémoire pour une table de similarité,
+ * ouvre le fichier de descripteur audio en mode lecture, lit les valeurs de ce fichier et les utilise pour créer un descripteur audio pour chaque fichier de comparaison,
+ * utilise une fonction "comparaison" pour calculer le pourcentage de similarité entre les deux descripteurs audio,
+ * stock ces pourcentages dans la table de similarité, trie les fichiers par ordre de similarité, et retourne la table de similarité.
+ *
+ * @param fenetre
+ * @param intervalle
+ * @param chemin_descripteur_compare
+ * @param chemin_descripteur_audio
+ * @param erreur
+ * @param Nb_ligne
+ * @return tab_similaire*
+ */
+tab_similaire *comparaison_audio(int fenetre, int intervalle, char *chemin_descripteur_compare, char *chemin_descripteur_audio, int *erreur, int *Nb_ligne);
 
-tab_similaire* comparaison_audio(int fenetre, int intervalle, char *chemin_descripteur_compare, char *chemin_descripteur_audio,int *erreur,int *Nb_ligne);
+tab_similaire *comparaison_texte(int nbr_mot, char *chemin_fichier_a_compare, char *chemin_descripteur, int *Erreur, int *Nb_ligne);
 
-
-tab_similaire* comparaison_texte(int nbr_mot,char* chemin_fichier_a_compare,char* chemin_descripteur,int * Erreur, int* Nb_ligne);
-
-
-//typedef enum {texte,image,audio}type_compare;
+// typedef enum {texte,image,audio}type_compare;
 
 #endif
--- descripteur_audio/descripteur_audio.h
+++ descripteur_audio/descripteur_audio.h
@@ -12,31 +12,34 @@ typedef struct
     int **tab;
 } descri_audio;
 
-
 /**
- * @brief 
- * 
- * elle permet de creer un descripteur d'un fichier txt tirer d'un fichier audio
- * 
- * @param fenetre 
- * @param intervalle 
- * @param chemin_fichier 
- * @param erreur 
- * @return descri_audio 
- * 
- * 
- * 
+ * @brief
+ *
+ * Elle utilise la fonction "getligne" pour obtenir le nombre de lignes dans le fichier,
+ * calcule le nombre de fenêtres en divisant le nombre de lignes par "nb_val_fenetre",
+ * crée une structure "descri_audio" pour stocker les données du descripteur audio, alloue de la mémoire pour cette structure, initialise les valeurs de cette structure à zéro,
+ * ouvre le fichier en mode lecture,
+ * lit les valeurs et les stockent dans la structure en utilisant une logique de comparaison pour déterminer à quelle intervalle chaque valeur appartient,
+ * ferme le fichier et retourne la structure "descri_audio" qui contient le descripteur audio.
+ *
+ * @param fenetre
+ * @param intervalle
+ * @param chemin_fichier
+ * @param erreur
+ * @return descri_audio
  */
-descri_audio Descripteur_audio(int fenetre, int intervalle, char *chemin_fichier,int * erreur);
+descri_audio Descripteur_audio(int fenetre, int intervalle, char *chemin_fichier, int *erreur);
 
 /**
- * @brief 
- * 
- * a quoi elle sert
- * 
+ * @brief
+ *
+ *  Elle utilise une commande "wc -l" pour compter le nombre de lignes dans le fichier spécifié par "path",
+ *  enregistre le résultat dans un fichier temporaire "getligne_temp" et l'ouvre ensuite pour lire le résultat.
+ *  Si le fichier est vide, elle assigne 7 à "erreur". Enfin, elle supprime le fichier temporaire et renvoie le nombre de lignes lues.
+ *
  * @param path chemin du fichier
- * @param erreur 
- * @return int 
+ * @param erreur
+ * @return int
  */
 int getligne(char *path, int *erreur);
 
