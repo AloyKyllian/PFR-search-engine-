3228c5b5c7ff8b8d683892fd22b1dbcd1d5bf572
Youssera A <achachera.Youssera@gmail.com>
2023-03-10 16:04:03 +0100

    suppression des fichier test JNA

--- exempleJNA/my-project/src/main/java/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-import com.sun.jna.ptr.PointerByReference;
-
-public class Main {
-    public static void main(String[] args) {
-        
-        System.out.println(testLibrary.INSTANCE.test(););
-
-    }
-}
--- exempleJNA/my-project/src/src/main/resources/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "test.h"
-
-void test(){
-    printf("test JNA\n");
-}
