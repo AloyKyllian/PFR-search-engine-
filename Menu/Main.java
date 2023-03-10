1cab2a062cac0c8de3da560403252611066dfdad
Youssera A <achachera.Youssera@gmail.com>
2023-03-10 16:53:27 +0100

    test JNA main

--- Menu/Main.java
+++ Menu/Main.java
@@ -1,18 +1,40 @@
-import com.sun.jna.ptr.PointerByReference;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.Memory;
 
-public class Main {
-    public static void main(String[] args) {
-        PointerByReference cfg = new PointerByReference();
-        int[] err_img = new int[1];
-        int[] err_aud = new int[1];
-        int[] err_txt = new int[1];
-        int[] err_idx = new int[1];
-        int[] erreurConfig = new int[1];
-        char[] choix = new char[100];
-        MenuLibrary.INSTANCE.Afficher_Menu(cfg);
-        MenuLibrary.INSTANCE.Lire_CHOIX(choix);
-        CONFIG config = MenuLibrary.INSTANCE.Lire_CONFIG(erreurConfig);
-        MenuLibrary.INSTANCE.indexation(config, err_img, err_aud, err_txt, err_idx);
-        MenuLibrary.INSTANCE.MAE(cfg, choix, err_img, err_aud, err_txt, err_idx);
-    }
+public class Main{
+
+public interface MenuLibrary extends Library {
+
+    MenuLibrary INSTANCE = (MenuLibrary) Native.loadLibrary("Menu.so", MenuLibrary.class);
+
+    void MAE(Pointer config, char [] choix, Pointer erreurImage,Pointer erreurAudio, Pointer erreurTexte, Pointer erreurIndex);
+}
+public static void main(String[] args) {
+    Congif config ;
+    Pointer ptrConfig=new Memory(4);
+    ptrConfig.setConfig(0,config);
+
+    int erreurImage = -1;
+    Pointer ptrErreurImage=new Memory(4);
+    ptrErreurImage.setInt(0,erreurImage);
+
+    int erreurAudio = -1;
+    Pointer ptrErreurAudio=new Memory(4);
+    ptrErreurAudio.setInt(0,erreurAudio);
+
+    int erreurIndex = -1;
+    Pointer ptrErreurIndex=new Memory(4);
+    ptrErreurIndex.setInt(0,erreurIndex);
+
+    int erreurTexte = -1;
+    Pointer ptrErreurTexte=new Memory(4);
+    ptrErreurTexte.setInt(0,erreurTexte);
+
+    char[] choix = new char[100];
+
+    MAE(ptrConfig,choix, ptrErreurImage, ptrErreurAudio, ptrErreurTexte, ptrErreurIndex);
+   
+}
 }
