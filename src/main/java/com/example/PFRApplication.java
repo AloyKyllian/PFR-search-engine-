7c57cf6791d9b47eca71d02c21f142ebe70c7bcc
Youssera A <achachera.Youssera@gmail.com>
2023-03-30 11:46:43 +0200

    control Image presque done

---
+++ src/main/java/com/example/PFRApplication.java
@@ -0,0 +1,24 @@
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class PFRApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PFRApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.setTitle("Bienvenue chez ID-HYLYK !");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/module-info.java
@@ -0,0 +1,8 @@
+module name {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.sql;
+    requires java.desktop;
+    requires jdk.jfr;
+}
